1794480185c0fa5abe5b78662f541f348a6a90eb
Brylian Foronda <foronda@hawaii.edu>
2012-11-12 14:54:00 -1000

    Fresh Commit of mygrades test
    This repo is used to get used to and understand the process of Ruby on
    Rails Application

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MygradesSr::Application.config.secret_token = 'e5b1d335245f36a88126eced28f425e57743bb83c427c647ecc483e5509cd3f2c3eabbb768c87c9407bc29901fb9324416ce6478e6a2d1da877d12b4a4ab2330'
