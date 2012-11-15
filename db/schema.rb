db4efac9046cd20e653cc4b6efc8033e7baf18ce
Brylian Foronda <foronda@hawaii.edu>
2012-11-15 12:03:49 -1000

    Updated Categories Migration, Modified Tasks Display Mode

--- db/schema.rb
+++ db/schema.rb
@@ -13,13 +13,6 @@
 
 ActiveRecord::Schema.define(:version => 20121115081608) do
 
-  create_table "categories", :force => true do |t|
-    t.string   "name"
-    t.float    "weight"
-    t.datetime "created_at", :null => false
-    t.datetime "updated_at", :null => false
-  end
-
   create_table "courses", :force => true do |t|
     t.integer  "task_id"
     t.string   "name"
