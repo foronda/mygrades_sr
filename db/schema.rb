dafae4f14e243930a1a306f43393d964fbdf76be
Brylian Foronda <foronda@hawaii.edu>
2012-11-15 12:10:18 -1000

    Updated Categories Migrataion date for db:migrate

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,14 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20121115081608) do
+ActiveRecord::Schema.define(:version => 20121115220531) do
+
+  create_table "categories", :force => true do |t|
+    t.string   "name"
+    t.float    "weight"
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
+  end
 
   create_table "courses", :force => true do |t|
     t.integer  "task_id"
