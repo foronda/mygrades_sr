91b0f42bf68fb5ea0cb00ea45219d4d9c82581ab
Brylian Foronda <foronda@hawaii.edu>
2012-11-26 00:38:39 -1000

    Added Google charts
    Make sure to require 'gchart' and restart server

--- Gemfile
+++ Gemfile
@@ -1,6 +1,7 @@
 source 'https://rubygems.org'
 
 gem 'rails', '3.2.9'
+gem "googlecharts", :require => "gchart"
 
 # TabsOnRails Gem Dependency
 gem 'tabs_on_rails'
@@ -26,7 +27,6 @@ group :assets do
   gem 'sass-rails',   '~> 3.2.3'
   gem 'coffee-rails', '~> 3.2.1'
 	gem 'bootstrap-sass', '~> 2.1.1.0'
-	
   # See https://github.com/sstephenson/execjs#readme for more supported runtimes
   # gem 'therubyracer'
 
--- app/helpers/students_helper.rb
+++ app/helpers/students_helper.rb
@@ -2,6 +2,9 @@ module StudentsHelper
 	def calc_homeworks(student_id)
 		@h_total = 0
 		@h_earned = 0
+		hName = []
+		hPoints = []
+		hEarned = []
 		
 		data ="<td><div id='homework' class='modal hide fade' tabindex='-1' role='dialog' aria-labelledby='myModalLabel' aria-hidden='true'>"
 		data += "<div class='modal-header'><h3 id='homeworkLabel'> Homework Breakdown</h3></div>"
@@ -10,14 +13,29 @@ module StudentsHelper
 			Task.find_all_by_id_and_category_id(grade.task_id, 1).each do |homework|
 					@h_total += homework.total
 					@h_earned += grade.earned
+					hName << homework.name
+					hPoints << homework.total
+					hEarned << grade.earned
 					data += "#{homework.name} - #{grade.earned}/#{homework.total}</br>"
 			end
 		end
-
+		#hEarned << @h_total
+		#hName << "Total"
+						
+		@graph = Gchart.bar(:data => [hEarned, hPoints], 
+												:axis_with_labels => ['x', 'y'],
+												:axis_labels => [hName],
+												:legend => ['Earned', 'Total'],
+												:bar_width_and_spacing => '25,40',
+												:bg => {:color => '000000'}, 
+												:bar_colors => 'ff0000,00ff00',
+												:max_value => 300,
+												:axis_range => [[150], [2,17,5]])
 		if(@h_total != 0)
-				data += "</br>Total #{((@h_earned.to_f/@h_total.to_f)*100).round}% (#{@h_earned}/#{@h_total})"
+			data += "</br>Total #{((@h_earned.to_f/@h_total.to_f)*100).round}% (#{@h_earned}/#{@h_total})"
+			data += "</br><img src=#{@graph}/></br>"
 			data += "</p></div></div>"
-			data += "Total #{((@h_earned.to_f/@h_total.to_f)*100).round}% (#{@h_earned}/#{@h_total})</td>"								
+			data += "Total #{((@h_earned.to_f/@h_total.to_f)*100).round}% (#{@h_earned}/#{@h_total})</td>"
 		else
 			data +="</td>"
 		end
