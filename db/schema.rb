5fe7a9bdd15aba2625b9020dde70e96ee05e4ec5
Brylian Foronda <foronda@hawaii.edu>
2012-11-30 12:04:16 -1000

    Added sessions table

--- db/schema.rb
+++ db/schema.rb
@@ -12,7 +12,7 @@
 # It's strongly recommended to check this file into your version control system.
 
 
-ActiveRecord::Schema.define(:version => 20121115231020) do
+ActiveRecord::Schema.define(:version => 20121130220040) do
 
   create_table "categories", :force => true do |t|
     t.string   "name"
@@ -53,6 +53,16 @@ ActiveRecord::Schema.define(:version => 20121115231020) do
   add_index "grades", ["course_id"], :name => "index_grades_on_course_id"
   add_index "grades", ["student_id"], :name => "index_grades_on_student_id"
   add_index "grades", ["task_id"], :name => "index_grades_on_task_id"
+
+  create_table "sessions", :force => true do |t|
+    t.string   "session_id", :null => false
+    t.text     "data"
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
+  end
+
+  add_index "sessions", ["session_id"], :name => "index_sessions_on_session_id"
+  add_index "sessions", ["updated_at"], :name => "index_sessions_on_updated_at"
 
   create_table "students", :force => true do |t|
     t.integer  "course_id"
