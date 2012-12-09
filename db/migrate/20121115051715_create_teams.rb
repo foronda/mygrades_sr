fca03d096f4863f4dc16d0aa850a22a8d6c24d16
Brylian Foronda <foronda@hawaii.edu>
2012-12-08 15:02:42 -1000

    Added teams population in migration

--- db/migrate/20121115051715_create_teams.rb
+++ db/migrate/20121115051715_create_teams.rb
@@ -7,5 +7,10 @@ class CreateTeams < ActiveRecord::Migration
       t.timestamps
     end
     add_index :teams, :course_id
+	
+	#Populate Database with teams for the course EE467
+	Teams.create :name => "SneakyRedondos", :course_id => Course.find_by_name("EE467").id
+	Teams.create :name => "Freshman FTW", :course_id => Course.find_by_name("EE467").id
+	Teams.create :name => "Hamburger", :course_id => Course.find_by_name("EE467").id
   end
 end
