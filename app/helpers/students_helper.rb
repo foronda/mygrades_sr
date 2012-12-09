463a9c5a604fc43654d45347d6f70794ae249de3
Brylian Foronda <foronda@hawaii.edu>
2012-12-08 21:27:55 -1000

    Created grades folder, and fixed calc_midterms helper

--- app/helpers/students_helper.rb
+++ app/helpers/students_helper.rb
@@ -29,7 +29,7 @@ module StudentsHelper
             
             # Generate google chart if homework exists and create image link
             data += generate_lower_modal_div(generate_chart(hHash, hLegend))
-            data += "Total #{calc_percentage(@h_earned, @h_total)}% (#{@h_earned}/#{@h_total})</td>"
+            data += "#{calc_percentage(@h_earned, @h_total)}% (#{@h_earned}/#{@h_total})</td>"
         else
             data +="</td>"
         end
@@ -44,45 +44,34 @@ module StudentsHelper
         @l_total = 0
         @l_earned = 0
 
-        lName = []
-        lGrade = []
+        lHash = Hash.new()
         lLegend = []
 
-        data ="<td><div id='lab' class='modal hide fade' tabindex='-1' role='dialog' aria-labelledby='myModalLabel' aria-hidden='true'>"
-        data += "<div class='modal-header'><h3 id='labLabel'> Lab Breakdown</h3></div>"
-        data += "<div class='modal-body'><p>"
+        data = generate_upper_modal_div("lab")
         Grade.find_all_by_student_id(student_id).each do |grade|
             Task.find_all_by_id_and_category_id(grade.task_id, 2).each do |lab|
                     @l_total += lab.total
                     @l_earned += grade.earned
-                    lName << lab.name
-                    # Calls helper functions
-                    lGrade << calc_percentage(grade.earned, lab.total)
+
                     lLegend << generate_legend(lab, grade)
+                    # Stores values into a hash
+                    # Keys = homework name
+                    # Values = grade percentage
+                    lHash[lab.name] = grade.earned.percent_of(lab.total)
             end
         end
 
-        if(@l_total != 0)
+        if(!lHash.empty?)
             # Generates total field data
-            lGrade << calc_percentage(@h_earned, @h_total)
-            lName << "Total"
-            lLegend << "Lab Total - #{calc_percentage(@l_earned, @l_total)}% (#{@l_earned}/#{@l_total})"
-
-            # Generate graph if lab exists...
-            @graph = Gchart.bar(:size => '400x200',
-                                :bar_colors => '76A4FB',
-                                :background => 'EEEEEE',
-                                :data => lGrade, 
-                                :axis_with_labels => ['x', 'y'],
-                                :axis_labels => [lName],
-                                :legend => lLegend, 
-                                :bar_width_and_spacing => '40,30',
-                                :bg => {:color => 'FFFFFF', :type => 'solid'}, 
-                                :encoding => 'text')
-            # Create an image link
-            data += "</br><img src=#{@graph}/></br>"
-            data += "</p></div></div>"
-            data += "Total #{calc_percentage(@l_earned, @l_total)}% (#{@l_earned}/#{@l_total})</td>"
+            lHash["Total"] = calc_percentage(@h_earned, @h_total)
+            lLegend << "Lab Total - #{@l_earned.percent_of(@l_total)}% (#{@l_earned}/#{@l_total})"
+            
+            # Sort hash alphabetically
+            lHash.keys.sort
+            
+            # Generate google chart if homework exists and create image link
+            data += generate_lower_modal_div(generate_chart(lHash, lLegend))
+            data += "#{@l_earned.percent_of(@l_total)}% (#{@l_earned}/#{@l_total})</td>"
         else
             data +="</td>"
         end
@@ -90,20 +79,41 @@ module StudentsHelper
     end
 
     def calc_midterms(student_id)
-        @m_total = 0
-        @m_earned = 0
-        Grade.find_all_by_student_id(student_id).each do |grade|
-            Task.find_all_by_id_and_category_id(grade.task_id, 3).each do |midterm|
-                    @m_total += midterm.total
-                    @m_earned += grade.earned
-            end
-        end
+      @m_total = 0
+      @m_earned = 0
 
-        if(@m_total != 0)
-            raw("<td> #{((@m_earned.to_f/@m_total.to_f)*100).round}% (#{@m_earned}/#{@m_total})</td>" ) 
-        else
-            raw("<td>N/A</td>")
-        end
+      mHash = Hash.new()
+      mLegend = []
+
+      data = generate_upper_modal_div("midterm")
+      Grade.find_all_by_student_id(student_id).each do |grade|
+        Task.find_all_by_id_and_category_id(grade.task_id, 3).each do |midterm|
+              @m_total += lab.total
+              @m_earned += grade.earned
+
+              mLegend << generate_legend(midterm, grade)
+              # Stores values into a hash
+              # Keys = homework name
+              # Values = grade percentage
+              mHash[midterm.name] = grade.earned.percent_of(midterm.total)
+          end
+      end
+
+      if(!mHash.empty?)
+          # Generates total field data
+          mHash["Total"] = calc_percentage(@m_earned, @m_total)
+          mLegend << "Midterm Total - #{@m_earned.percent_of(@m_total)}% (#{@m_earned}/#{@m_total})"
+          
+          # Sort hash alphabetically
+          mHash.keys.sort
+          
+          # Generate google chart if homework exists and create image link
+          data += generate_lower_modal_div(generate_chart(mHash, mLegend))
+          data += "#{@m_earned.percent_of(@m_total)}% (#{@m_earned}/#{@m_total})</td>"
+      else
+          data +="</td>"
+      end
+      data.html_safe
     end
 
     def calc_course(student_id)
